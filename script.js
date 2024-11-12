10fd615dec73b298c8a678e5e5fa4539b611412f
SERENA FERRARO <serenaferraro21@gmail.com>
2024-11-12 15:19:48 +0100

    esercizio 1

--- script.js
+++ script.js
@@ -1,18 +1,58 @@
-const firstName = document.getElementById("name");
-const email = document.getElementById("email");
+const nome = document.getElementById("nome");
+const cognome = document.getElementById("cognome");
 const invia = document.getElementById("btn-invia");
-const elimina = document.getElementById("btn-elimina");
+const elimina = document.getElementById('btn-elimina');
+const cardsContainer = document.getElementById('cards');
 
 class User {
-  constructor(_firstName, _email) {
-    this.name = _firstName;
-    this.email = _email;
+  constructor(_nome, _cognome) {
+    this.nome = _nome;
+    this.cognome = _cognome;
   }
 }
 
-const getValues = () => {
-  const newUser = new User(firstName.value, email.value)
-  console.log(newUser)
+// Create a new card element
+const createCard = (user) => {
+  const newCard = document.createElement('div');
+  newCard.classList.add('border', 'border-2', 'border-info', 'row', 'm-3');
+  newCard.innerHTML = `<div class="card">
+    <div class="card-body">
+      <h5 class="card-title">User</h5>
+      <p class="card-text">${user.nome} ${user.cognome}</p>
+    </div>
+  </div>`;
+  cardsContainer.appendChild(newCard);
 };
 
-getValues()
+
+const loadCards = () => {
+  const users = JSON.parse(localStorage.getItem('users')) || [];
+  users.forEach(user => createCard(user));
+};
+
+
+const getValues = () => {
+  const newUser = new User(nome.value, cognome.value);
+  createCard(newUser);
+
+  const users = JSON.parse(localStorage.getItem('users')) || [];
+  users.push(newUser);
+  localStorage.setItem('users', JSON.stringify(users));
+};
+
+const eliminaElemento = () => {
+  localStorage.removeItem('users'); 
+  cardsContainer.innerHTML = '';     
+};
+window.addEventListener('load', loadCards);
+
+
+invia.addEventListener('click', (e) => {
+  e.preventDefault();
+  getValues();
+});
+
+elimina.addEventListener('click', (e) => {
+  e.preventDefault();
+  eliminaElemento();
+});
